e2596b5ec6c757d3c4b48481344075a25690bae9
Omid Habibi <omid_h28@hotmail.com>
2020-09-08 12:07:05 +0430

    Add customer permissions

--- src/team-permissions.enum.ts
+++ src/team-permissions.enum.ts
@@ -2,6 +2,8 @@ export enum TeamPermission {
   OWNERSHIP = 'ownership',
   READ_STORAGE = 'r_storage',
   WRITE_STORAGE = 'w_storage',
+  READ_CUSTOMER = 'r_customer',
+  WRITE_CUSTOMER = 'w_customer',
 }
 
 export function getAllTeamPermissions(): TeamPermission[] {
