b8f56f4f7ad012c70c31ab5d5e69ede2437d7228
Omid Habibi <omid_h28@hotmail.com>
2020-10-12 06:28:58 +0330

    Remove promotion and write-storage permissions

--- src/team-permissions.enum.ts
+++ src/team-permissions.enum.ts
@@ -1,13 +1,11 @@
 export enum TeamPermission {
   OWNERSHIP = 'ownership',
   ASSISTANCE = 'assistance',
-  READ_STORAGE = 'r_storage',
   WRITE_STORAGE = 'w_storage',
   READ_CUSTOMER = 'r_customer',
   WRITE_CUSTOMER = 'w_customer',
   READ_ORDER = 'r_order',
   WRITE_ORDER = 'w_order',
-  MANAGE_PROMOTION = 'm_promotion',
 }
 
 export function getAllTeamPermissions(): TeamPermission[] {
