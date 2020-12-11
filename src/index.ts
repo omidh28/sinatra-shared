a0f9b9b74ecdf9ee9bea8542d13ff332ba898e07
Omid Habibi <omid_h28@hotmail.com>
2020-12-11 17:06:23 +0330

    Add invoice payload

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './team-settings.interface';
 export * from './sms-provider.enum';
 export * from './feedback.payload';
 export * from './force-update.middleware';
+export * from './invoice.payload';
---
+++ src/invoice.payload.ts
@@ -0,0 +1,8 @@
+export type InvoicePayload = {
+  readonly customerName: string;
+  readonly customerPhone: string;
+  readonly price: string;
+  readonly quantity: string;
+  readonly models: string[];
+  readonly advancePay: number;
+}
