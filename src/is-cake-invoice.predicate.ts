bc6fc7e0e4ec8a92e07d59476c10b9e040f726e4
Omid Habibi <omid_h28@hotmail.com>
2021-04-21 15:11:54 +0430

    Fix typo

--- src/is-cake-invoice.predicate.ts
+++ src/is-cake-invoice.predicate.ts
@@ -1,7 +1,7 @@
 import { CakeOrderInvoicePayload } from "./cake-order-invoice.payload";
 import { DessertOrderInvoicePayload } from "./dessert-order-invoice.payload";
 
-export function isCreateCakeOrderOptions(
+export function IsCakeOrderInvoice(
   options: CakeOrderInvoicePayload | DessertOrderInvoicePayload,
 ): options is CakeOrderInvoicePayload {
   return 'quantity' in options;
